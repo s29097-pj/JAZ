c3c30f845677d9fd8b73fe58e0ed3e0f83550f12
s29097-pj <115408708+s29097-pj@users.noreply.github.com>
2024-05-30 12:54:43 +0200

    Update MovieController.java

--- Zjazd_04/MovieService/src/main/java/pl/pjatk/movieservice/controller/MovieController.java
+++ Zjazd_04/MovieService/src/main/java/pl/pjatk/movieservice/controller/MovieController.java
@@ -1,6 +1,8 @@
 package pl.pjatk.movieservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +19,8 @@ public class MovieController {
     private MovieRepository movieRepository;
 
     @GetMapping
-    public List<Movie> getAllMovies() {
-        return movieRepository.findAll();
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        List<Movie> movies = movieRepository.findAll();
+        return new ResponseEntity<>(movies, HttpStatus.OK);
     }
 }
