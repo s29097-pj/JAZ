84477668b57faebebe6a480654a12af0a0d18219
s29097-pj <115408708+s29097-pj@users.noreply.github.com>
2024-05-31 20:15:16 +0200

    Update MovieService.java

--- Zjazd_04/MovieService/src/main/java/pl/pjatk/movieservice/service/MovieService.java
+++ Zjazd_04/MovieService/src/main/java/pl/pjatk/movieservice/service/MovieService.java
@@ -1,10 +1,12 @@
 package pl.pjatk.movieservice.service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import pl.pjatk.movieservice.model.Movie;
 import pl.pjatk.movieservice.repository.MovieRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -15,8 +17,53 @@ public class MovieService {
         this.movieRepository = movieRepository;
     }
 
-    public List<Movie> findAll() {
+    public List<Movie> getAllMovies() {
         return movieRepository.findAll();
+    }
+
+    public Movie getMovieById(int id) {
+        Optional<Movie> movie = movieRepository.findById((long) id);
+        return movie.orElse(null);
+    }
+
+    public ResponseEntity<Movie> addMovie(Movie movie) {
+        Movie savedMovie = movieRepository.save(movie);
+        return ResponseEntity.ok(savedMovie);
+    }
+
+    public ResponseEntity<Movie> updateMovie(int id, Movie movie) {
+        Optional<Movie> existingMovie = movieRepository.findById((long) id);
+        if (existingMovie.isPresent()) {
+            Movie updatedMovie = existingMovie.get();
+            updatedMovie.setName(movie.getName());
+            updatedMovie.setCategory(movie.getCategory());
+            movieRepository.save(updatedMovie);
+            return ResponseEntity.ok(updatedMovie);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    public ResponseEntity<Void> deleteMovie(int id) {
+        Optional<Movie> existingMovie = movieRepository.findById((long) id);
+        if (existingMovie.isPresent()) {
+            movieRepository.delete(existingMovie.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    public ResponseEntity<Movie> setMovieAvailability(Long id, boolean isAvailable) {
+        Optional<Movie> existingMovie = movieRepository.findById(id);
+        if (existingMovie.isPresent()) {
+            Movie movie = existingMovie.get();
+            movie.setIsAvailable(isAvailable);
+            movieRepository.save(movie);
+            return ResponseEntity.ok(movie);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
